47de06740bdca5745dde8e6a5d697889d1a38d43
derrickturk <dwt@terminusdatascience.com>
2018-05-30 14:54:08 -0500

    A dumb banner.

--- src/Main.hs
+++ src/Main.hs
@@ -7,43 +7,22 @@ import qualified Data.Text.IO as TIO
 
 import Language.JStlc.Types
 import Language.JStlc.Parse
-import Language.JStlc.Syntax
 import Language.JStlc.Check
 import Language.JStlc.Eval
 import Language.JStlc.Compile
 import Language.JStlc.JS
 
-example :: Term as ('ListTy 'IntTy)
-example = App (Lam "x" SIntTy (Cons (Var IZ) (Lit [3, 2, 1])))
-              (Lit 4)
-
-example2 :: Term as ('OptionTy 'BoolTy)
-example2 = MapOption (Lam "x" SBoolTy (BinOpApp Or (Var IZ) (Lit True)))
-                     (Some (BinOpApp Eq (Lit (17 :: Integer)) (Lit 21)))
-
 main :: IO ()
-main = do
-  print example
-  putStr "=eval=> "
-  print $ eval example
+main = banner >> repl
 
-  let js = compile example
-  putStr "=compile=> "
-  print js
-  putStr "=emit=> "
-  TIO.putStrLn $ emit js
-
-  print example2
-  putStr "=eval=> "
-  print $ eval example2
-
-  let js2 = compile example2
-  putStr "=compile=> "
-  print js2
-  putStr "=emit=> "
-  TIO.putStrLn $ emit js2
-
-  repl
+banner :: IO ()
+banner =
+  putStrLn "       __ ____  __   __" >>
+  putStrLn "      / // __/_/ /_ / /" >>
+  putStrLn "  __ / //_  //_  _// /___  v0.1" >>
+  putStrLn " / // /__/ /  / / / // _/  by dwt @ terminus data science, LLC" >>
+  putStrLn "/___ //___/  /_/ /_//__/   (c) 2018" >>
+  putStrLn ""
 
 repl :: IO ()
 repl = do
